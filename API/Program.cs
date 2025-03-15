5ab69f88c94f1aba0a31543103bbe6df77f8144b
Francisco Simões <s1moes>
2025-03-15 15:29:20 +0000

    try to add endpoints

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using MongoDB.Driver;
 using System.Text.Json.Serialization;
+using System.Diagnostics.CodeAnalysis;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -24,7 +25,13 @@ builder.Services.AddCors(options =>
                         .AllowAnyHeader());
 });
 
-builder.Services.AddControllers();
+[RequiresUnreferencedCode("MVC does not currently support trimming or native AOT.")]
+void AddControllersWithWarning(IServiceCollection services)
+{
+    services.AddControllers();
+}
+
+AddControllersWithWarning(builder.Services);
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
@@ -36,31 +43,3 @@ app.UseCors("AllowAllOrigins");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
-//public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-//[JsonSerializable(typeof(Todo[]))]
-//internal partial class AppJsonSerializerContext : JsonSerializerContext
-//{
-
-//}
-
-//builder.Services.ConfigureHttpJsonOptions(options =>
-//{
-//    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-//});
-
-//var sampleTodos = new Todo[] {
-//    new(1, "Walk the dog"),
-//    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-//    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-//    new(4, "Clean the bathroom"),
-//    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-//};
-
-//var todosApi = app.MapGroup("/todos");
-//todosApi.MapGet("/", () => sampleTodos);
-//todosApi.MapGet("/{id}", (int id) =>
-//    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-//        ? Results.Ok(todo)
-//        : Results.NotFound());

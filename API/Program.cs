6d09e39172b300cdffaf6bea02d906528ddca3b0
Francisco Simões <s1moes>
2025-03-17 13:35:14 +0000

    program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
-
-builder.Services.AddHealthChecks();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 app.UseHealthChecks("/health");
-
+app.UseRouting();
+app.UseCors("AllowAllOrigins");
 app.UseAuthorization();
 
 app.MapControllers();
